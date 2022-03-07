e4ba85fbad2369d2857c430dd9d931410504cd27
raegon <raegon@gmail.com>
2022-03-07 23:25:21 +0900

    spring-batch-jpa-without-meta

--- spring-batch-jpa-without-meta/src/main/java/com/raegon/example/batch/jpa/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.raegon.example.batch.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
